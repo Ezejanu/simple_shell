39e556795722d9134a432157cac244dc2e0f075d
Ezejanu <Ezejanu_nno@ymail.com>
2023-06-20 19:37:57 -0700

    setting up the repo

---
+++ README.md
@@ -0,0 +1,4 @@
+This is  a project to build a simple shell.
+The collaborators on this project are:
+Aishah Mabayoje
+Ezejanu Dike
---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+
+
+
+#endif
