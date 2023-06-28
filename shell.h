3b32e09145070ea4672af7a8496ac571a8740d67
Ezejanu <Ezejanu_nno@ymail.com>
2023-06-28 05:41:45 +0100

    new header file

---
+++ shell.h
@@ -0,0 +1,18 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <unistd.h>
+
+extern char **environ;
+/* char *prompt = "our shell $: "; */
+
+/*String Functions*/
+
+#endif
